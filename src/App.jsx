fbf59509c1d711767c5654fabaed848a7c5d1a97
Yam Clement <yamtzunhei@gmail.com>
2019-11-27 16:07:30 +0100

    file structure & routing

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import Root from "./screens/Root";
+
+const App = () => (
+  <div className="App">
+    <Root />
+  </div>
+);
+
+export default App;
